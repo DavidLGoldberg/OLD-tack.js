af71ecea4363788caced2a8e750e2b9a90211726
David Goldberg <DavidLGoldberg@gmail.com>
2012-08-11 03:10:31 -0400

    faster, smoother events to compete with angular.js's performance.

--- watch.js
+++ watch.js
@@ -60,7 +60,7 @@ if (!Object.prototype.unwatch) {
 var $w = {};
 
 var bindAllInputs = function() {
-    $('input[data-w]').on('change', function(e) {
+    $('input[data-w]').on('keyup keydown', function(e) {
         $w[$(this).data('w')] = $(e.target).val();
     });
 };
