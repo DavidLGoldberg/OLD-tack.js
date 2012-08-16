5ea2e676877d12d5d4fff7f72c4131a8f9f54bab
David Goldberg <DavidLGoldberg@gmail.com>
2012-08-16 09:16:37 -0400

    almost working.

--- tack.js
+++ tack.js
@@ -1,81 +1,43 @@
-var $w = {};
+window.TACK = {};
 
 var bindAllInputs = function() {
     $('input[data-w]').on('keydown keyup', function(e) {
-        $w[$(this).data('w')] = $(e.target).val();
+        TACK[$(this).data('tack-val')] = $(e.target).val();
     });
 };
 
-$w.init = function(options) {
+TACK.init = function(options) {
 
     var settings = $.extend({
         debug: false,
-        globalSetup: null,
-        renderings: [],
         data: {}
     }, options); 
 
-    $w = $.extend($w, settings.data);
+    TACK = $.extend(TACK, settings.data);
 
     $('body').find('[data-view]')
         .each(function() {
             var $this = $(this);
             var $view = $this.data('view');
 
-            if (typeof settings.globalSetup === 'function') { 
-                if (settings.debug) {
-                    alert($view +': globalSetup run');
-                }
-                settings.globalSetup($this);
-            }
-
             if (settings.debug) {
                 alert($view +': initial run');
             }
-            settings.renderings[$view]($w, $this);
         })
-
-    var renderingDependencies = {};
 
     for (var prop in settings.data)
     {
-        // Automatically detect dependencies:
-        for (var rendering in settings.renderings) {
-            var dependency;
-            if (settings.renderings[rendering].toString().indexOf('$w.' + prop) > -1) {
-                dependency = prop;
-            }
-
-	    if (renderingDependencies[rendering]) {
-	        renderingDependencies[rendering].push(dependency);
-	    }
-	    else {
-	        renderingDependencies[rendering] = new Array(dependency);
-	    }
-	}
-
         // Use native or polly filled .watch
-        $w.watch(prop, function (key, oldval, newval) {
+        TACK.watch(prop, function (key, oldval, newval) {
             
             if (settings.debug) {
                 console.log(key + " changed from " + oldval + " to " + newval);
             }
 
-            for (var viewName in settings.renderings)
-            {
-                var w = $.extend({}, $w);
-                w[key] = newval;
+            var newData = settings.data;
+            newData[key] = newval;
 
-                // check if rendering needs to be fired
-                if ($.inArray(key, renderingDependencies[viewName]) > -1) { 
-                    // Run rendering: 
-                    settings.renderings[viewName](w, $('[data-view=' + viewName + ']'));
-
-                    if (settings.debug) {
-                        console.log('rendering: ' + viewName + ' called');
-                    }
-                }
-            }
+            $('[data-tack-view]').trigger('tack-change', [newData]);
 
             return newval;
         });
